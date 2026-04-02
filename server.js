20b30cac39a265c2468ab1a397e5d89b9d07164c
SinghHarsh14777 <singhharsh14777@gmail.com>
2026-04-02 23:25:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173','https://auth-app66.netlify.app' credentials: true }));
 app.use(express.json());
 
 // Routes
